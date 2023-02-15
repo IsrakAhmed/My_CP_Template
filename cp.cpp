d14520b2da6f6677d3a8685a4d69bbe0be8f575a
Israk Ahmed <israkahmed7@gmail.com>
2023-02-15 17:52:01 +0600

    first cp template - added

---
+++ cp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define asort (v).begin(),(v).end()        // Ascending Order Sort
+#define dsort (v).rbegin(),(v).rend()     // Descending Order Sort
+#define pb push_back
+#define sumv v.begin(), v.end(), 0
+
+using namespace std;
+
+void solution(){
+
+}
+
+int main(){
+    solution();
+    return 0;
+}
