5ef8629ad9bfb3bd4534b6bdd3378ddc997fa745
Israk Ahmed <israkahmed7@gmail.com>
2023-03-17 19:37:09 +0600

    template update 4

--- cp.cpp
+++ cp.cpp
@@ -9,14 +9,14 @@
 
 #include <bits/stdc++.h>
 
+using namespace std;
+
 #define ll long long
 #define asort (v).begin(),(v).end()        // Ascending Order Sort
 #define dsort (v).rbegin(),(v).rend()     // Descending Order Sort
 #define pb push_back
 #define sumv v.begin(), v.end(), 0
 #define gcd __gcd
-
-using namespace std;
 
 ll random(ll low, ll high) {
     // Generate random number
