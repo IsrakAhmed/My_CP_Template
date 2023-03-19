4233fe5da5552c2e1b798f513651d8e39aef560a
Israk Ahmed <israkahmed7@gmail.com>
2023-03-19 13:31:46 +0600

    2021 regional file added

---
+++ 2021 ICPC ASIA DHAKA REGIONAL BUBT/J. ICPC Standing.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int testCases,i,p,s,r;
+
+    scanf("%d",&testCases);
+
+    for(i = 1; i <= testCases; i++){
+        scanf("%d%d%d",&p,&s,&r);
+        printf("Case %d: Yes\n",i);
+    }
+
+    return 0;
+}
