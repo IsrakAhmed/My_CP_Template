10af648fe37a6c0676ce67666e63f42d2819216a
Israk Ahmed <israkahmed7@gmail.com>
2023-03-31 23:53:03 +0600

    template update 6

--- cp.cpp
+++ cp.cpp
@@ -23,22 +23,29 @@ using namespace std;
 #define rmvChar(s,c) (s).erase(remove((s).begin(), (s).end(), c), (s).end())    //remove specific character from string
 #define rmvDuplicates(x) (x).erase(unique((x).begin(), (x).end()), (x).end());  //works only for sorted string
 #define all(x) (x).begin(),(x).end()
+#define nl "\n"
 
-ll random(ll low, ll high) {
+ll random(ll low, ll high)
+{
     // Generate random number
     return rand() % (high - low + 1) + low;
 }
 
-void solution(){
+void solution()
+{
     ll t;
     cin >> t;
 
-    while(t--){
+    while(t--)
+    {
 
     }
 }
 
-int main(){
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     solution();
     return 0;
 }
