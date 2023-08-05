67cc5dfbeeb45c37cc865101b26a41f9c5d18be1
Israk Ahmed <israkahmed7@gmail.com>
2023-08-05 19:38:18 +0600

    template update 7

--- cp.cpp
+++ cp.cpp
@@ -44,8 +44,9 @@ void solution()
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     solution();
     return 0;
 }
